d296124c8715b9dbc4a140c785641ce41e351a0d
Santiago Pegels <santiagopegels@gmail.com>
2021-05-17 10:53:53 -0300

    server config

--- app.ts
+++ app.ts
@@ -1,3 +1,8 @@
-export const nombre = 'Santiago'
+import dotenv from "dotenv";
+import Server from "./models/server";
+dotenv.config()
 
-console.log(nombre);
+
+const server = new Server()
+
+server.listen()
