770335a14629cfb17d53dc0e87a06056c0c30d08
Santiago Pegels <santiagopegels@gmail.com>
2021-05-18 11:41:42 -0300

    add update user method

--- controllers/users.ts
+++ controllers/users.ts
@@ -2,11 +2,10 @@ import { Request, Response } from "express";
 import User from "../models/user";
 
 export const getUsers = async (req: Request, res: Response) => {
-
   const users = await User.findAll();
 
   res.json({
-    users
+    users,
   });
 };
 
@@ -15,10 +14,10 @@ export const getUser = async (req: Request, res: Response) => {
 
   const user = await User.findByPk(id);
 
-  if(!user){
+  if (!user) {
     return res.status(404).json({
-      msg: 'No existe el usuario con el id ' + id
-    })
+      msg: "No existe el usuario con el id " + id,
+    });
   }
 
   res.json({
@@ -26,27 +25,69 @@ export const getUser = async (req: Request, res: Response) => {
   });
 };
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
   const { body } = req;
-  res.json({
-    msg: "createUser",
-    body,
-  });
+
+  try {
+    const existeEmail = await User.findOne({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (existeEmail) {
+      return res.status(400).json({
+        msg: "Ya existe el email que intenta registrar",
+      });
+    }
+
+    const user = new User(body);
+    await user.save();
+
+    res.json({
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "Ocurrió un error",
+    });
+  }
 };
 
-export const updateUser = (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const {body} = req
-  res.json({
-    msg: "getUser",
-    id,
-  });
+  const { body } = req;
+
+  try {
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({
+        msg: "No existe un usuario con el id " + id,
+      });
+    }
+
+    await user.update(body)
+
+    res.json({
+      msg: "Usuario actualizado correctamente",
+      user
+    })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "Ocurrió un error",
+    });
+  }
 };
 
 export const deleteUser = (req: Request, res: Response) => {
-    const { id } = req.params;
-    res.json({
-      msg: "deleteUser",
-      id
-    });
-  };
+  const { id } = req.params;
+  res.json({
+    msg: "deleteUser",
+    id,
+  });
+};
